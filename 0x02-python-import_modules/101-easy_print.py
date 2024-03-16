8074bfa75422054e8a79c18e8d98f18d2a96d789
Shengero4 <sdauceedouglas@gmail.com>
2024-03-17 00:30:41 +0200

    rachel

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,4 +1,2 @@
 #!/usr/bin/python3
-# Use getattr to access the built-in namespace and get the 'id' function if available,
-# otherwise use the 'print' function
 getattr(__builtins__, '__dict__', {}).get('id', print)("#pythoniscool")
