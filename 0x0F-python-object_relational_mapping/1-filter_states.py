76a25f15099501dfe1f0f8242579b30224b66530
Shengero4 <sdauceedouglas@gmail.com>
2024-06-26 12:04:48 +0300

    second

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,40 @@
+Les raccourcis clavier vont changer … Le jeudi 1 août 2024, les raccourcis clavier de Drive seront modifiés pour que vous puissiez naviguer avec les premières lettresEn savoir plus
+1-filter_states.py
+#!/usr/bin/python3
+"""
+Lists all states with a name starting with N (upper N) from the database
+hbtn_0e_0_usa sorted in ascending order by states.id
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    try:
+        conn = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=mysql_username,
+            passwd=mysql_password,
+            db=db_name,
+            charset="utf8"
+        )
+    except MySQLdb.Error as e:
+        print("Error connecting to database: {}".format(e))
+        sys.exit(1)
+
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' \
+                ORDER BY states.id ASC")
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    conn.close()
